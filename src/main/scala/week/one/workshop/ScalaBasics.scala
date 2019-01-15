98553c0df7f53f69834541804c42f7c067d1a15f
robertbutacu <robertbutacu@gmail.com>
2019-01-15 22:33:57 +0000

    wrong file whoops

--- src/main/scala/week/one/workshop/ScalaBasics.scala
+++ src/main/scala/week/one/workshop/ScalaBasics.scala
@@ -1,5 +1,25 @@
 package week.one.workshop
 
 class ScalaBasics {
+  //what is a var/val + difference
 
+  //type inference
+
+  //how to declare a function
+
+  //lazy/eager
+
+  // object
+
+  //class / case class
+
+  //trait / abstract class
+
+  //extends, with
+
+  //sealed
+
+  //imports
+
+  //sbt clean compile, sbt test, talk about build file
 }
