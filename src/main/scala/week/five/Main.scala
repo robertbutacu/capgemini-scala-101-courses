ac6392c2b0f666efdda86e97386b7cc0a07b9dd8
robertbutacu <robertbutacu@gmail.com>
2019-01-15 22:33:06 +0000

    added agenda for first week

--- src/main/scala/week/five/Main.scala
+++ src/main/scala/week/five/Main.scala
@@ -2,4 +2,25 @@ package week.five
 
 class Main {
 
+  //what is a var/val + difference
+
+  //type inference
+
+  //how to declare a function
+
+  //lazy/eager
+
+  // object
+
+  //class / case class
+
+  //trait / abstract class
+
+  //extends, with
+
+  //sealed
+
+  //imports
+
+  //sbt clean compile, sbt test, talk about build file
 }
