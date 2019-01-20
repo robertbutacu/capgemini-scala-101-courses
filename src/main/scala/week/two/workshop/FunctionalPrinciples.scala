cd87c8a4900e901b21c9cab7514c021a012b5728
robertbutacu <robertbutacu@gmail.com>
2019-01-20 21:07:22 +0000

    agenda for week 2

--- src/main/scala/week/two/workshop/FunctionalPrinciples.scala
+++ src/main/scala/week/two/workshop/FunctionalPrinciples.scala
@@ -5,15 +5,45 @@ object FunctionalPrinciples extends App {
 
   // pureness + referential transparency
 
+  // what implies to be pure ?
+  // what implies to be referential transparent ?
+  // function which is impure
+  //transform it so it is pure => simple division after asking for input from user
+
+
+
   // functions + function composition
 
+  // explain what functions are in scala
+
+  // andThen example
+
+  // brewing coffee example
+  // 3 functions which take 1 string and return a string
+  // compose them
+
+
   // recursiveness + @tailrec
+  // modelling every iterative problem as a recursive one
+  // function which takes Int and returns a list with elements up to that int
+
+  // problems with that
+  // @tailrec and memoization
+
+
 
   // types - generics and relevance
+  // function which takes Int, Double, Float and returns String
+  // make it generic
+
 
   // higher order functions
+  // examples
+
 
   // ADTs
+  // examples => traffic signaling light
 
   // why is this relevant
+  // testing, accuracy, stable
 }
